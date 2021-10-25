8e050f659ceae08941f4bdf38772404d3834c34a
Santi <it.santiagotoledo@gmail.com>
2021-10-25 02:24:07 -0300

    create web server

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict' 
+
+const express = require('express'); 
+const bodyParser = require('body-parser');
+
+const app = express(); //controlador/framework http
+
+// cargar rutas
+
+// body-parser
+
+app.use(express.urlencoded({extended:false})); //convert get/post/etc to json
+app.use(express.json());
+
+// configurar CORS
+
+
+// configurar rutas
+app.get('/pruebas-api', (req, res) => {
+    res.status(200).send({
+        message: "esta ruta es de prueba en mi api restful con mongo y node"
+    });
+})
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict'    //js es6
 
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const app = require('./app');
+const port = 3800;
 
 mongoose.connect('mongodb://localhost:27017/curso_mongo')
     .then(() => {
         console.log('La conexion a MongoDB se ha realizado correctamente!');
+        app.listen(port, () => {
+            console.log('El servidor esta corriendo en localhost: 3800')
+        });
     })
     .catch(err => console.log(err + ' no se que paso'));
